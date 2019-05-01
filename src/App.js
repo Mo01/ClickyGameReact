858fdd75999537633d25c402ff42fab91ae7b462
Mo <moh.alneama@yahoo.com>
2019-05-01 18:46:07 -0500

    aded imagse

--- src/App.js
+++ src/App.js
@@ -1,39 +1,86 @@
 import React, { Component } from 'react';
 import './App.css';
-import Todos from './components/Todos'
+import Cards from './components/Cards'
 
 class App extends Component {
 state = {
 
-todos: [
+Cards: [
 {
   id: 1,
-  title: "task1",
+  title: "./img/1.jpg",
   complete: false
 },
 {
   id: 2,
-  title: "task2",
+  title: "./img/2.jpg",
   complete: false
 },
 {
   id: 3,
-  title: "task3",
+  title: "./img/3.jpg",
+  complete: false
+},
+{
+  id: 4,
+  title: "./img/4.jpg",
+  complete: false
+},
+{
+  id: 5,
+  title: "./img/5.jpg",
+  complete: false
+},
+{
+  id: 6,
+  title: "./img/6.jpg",
+  complete: false
+},
+{
+  id: 7,
+  title: "./img/7.jpg",
+  complete: false
+},
+{
+  id: 8,
+  title: "./img/8.jpg",
+  complete: false
+},
+{
+  id: 9,
+  title: "./img/9.jpg",
+  complete: false
+},
+{
+  id: 10,
+  title: "./img/10.jpg",
+  complete: false
+},
+{
+  id: 11,
+  title: "./img/11.jpg",
+  complete: false
+},
+{
+  id: 12,
+  title: "./img/12.jpg",
   complete: false
 }
+
 
 
 ]
 
 }
 
+
   render() {
-    // console.log("App.js: ", this.state.todos)
-    console.log(this.state.todos)
+    // console.log("App.js: ", this.state.Cards)
+    // console.log(this.state.Cards)
 
     return (
       <div className="App">
-     <Todos todos={this.state.todos}/>
+     <Cards Cards={this.state.Cards} />
       </div>
     );
   }
