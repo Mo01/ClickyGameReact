f47c1f65dd295c66f383d8eb5b38b8c235c662c4
Mo <moh.alneama@yahoo.com>
2019-05-02 21:46:49 -0500

    fixed reset funcntion

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,12 +1,16 @@
-import React from "react";
-import "./Card.css";
+import React from 'react';
+import './Card.css';
 
-const Card = props => (
-  <div className="card" onClick={() => props.clickCount(props.id)}>
-    <div className="img-container">
-      <img src={props.image} />
-    </div>
-  </div>
+const divStyle = {
+	margin: '40px',
+	border: '5px solid red'
+};
+const Card = (props) => (
+	<div className="card" id={props.id} onClick={() => props.clickCount(props.id)}>
+		<div style={divStyle} className="img-container">
+			<img src={props.image} />
+		</div>
+	</div>
 );
 
 export default Card;
